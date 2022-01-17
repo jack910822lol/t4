53437fdfb6123a48a475f2dfe52280ef25ef964b
jack910822lol <32065912+jack910822lol@users.noreply.github.com>
2022-01-18 02:34:59 +0800

    :

---
+++ t4/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace t4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.man = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.man)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::t4.Properties.Resources.map;
+            this.pictureBox3.Location = new System.Drawing.Point(37, 146);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(191, 40);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "floor";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::t4.Properties.Resources.map;
+            this.pictureBox2.Location = new System.Drawing.Point(329, 280);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 41);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "floor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::t4.Properties.Resources.map;
+            this.pictureBox1.Location = new System.Drawing.Point(637, 418);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 41);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "floor";
+            // 
+            // man
+            // 
+            this.man.BackColor = System.Drawing.Color.Transparent;
+            this.man.Image = global::t4.Properties.Resources.stickman1;
+            this.man.Location = new System.Drawing.Point(461, 486);
+            this.man.Name = "man";
+            this.man.Size = new System.Drawing.Size(59, 77);
+            this.man.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.man.TabIndex = 3;
+            this.man.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(909, 563);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.man);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.man)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox man;
+    }
+}
+
---
+++ t4/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace t4
+{
+    public partial class Form1 : Form
+    {
+        int pow =0, g=-3;
+        int movent = 1, storepow = 2, move = 3, directionset = 4, onfloor = 5, state = 1, left = 6, right = 7, direct = 9, vertical = 9 ;
+        public Form1()
+        {
+            DoubleBuffered = true;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up&&state == movent)
+            {
+                state = storepow;
+            }
+            else if (e.KeyCode == Keys.Left && state == storepow)
+            {
+                Console.WriteLine("dire");
+                state = directionset;
+                direct = left;
+            }
+            else if (e.KeyCode == Keys.Right && state == storepow)
+            {
+                Console.WriteLine("dire");
+                state = directionset;
+                direct = right;
+            }
+            else if (e.KeyCode == Keys.Left && state == movent)
+            {
+                man.Left -= 10;
+            }
+            else if (e.KeyCode == Keys.Right && state == movent)
+            {
+                man.Left += 10;
+            }
+        }
+
+        public void iffell(PictureBox man,PictureBox floor)
+        {
+            if (man.Right < floor.Left || man.Left > floor.Right)
+                state = move;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int var=10;
+            if (state == storepow)
+            {
+                if(pow<=40)
+                    pow+=3;
+            }
+            if(state == move && direct ==vertical)
+            {
+                Console.WriteLine("move");
+                pow += g;
+                man.Top -= pow;
+                if(man.Top>=this.Height-100)
+                {
+                    pow = 0;
+                    state = movent;
+                    man.Top = this.Height - man.Height -30;
+                }
+            }
+            if (state == move && direct!= vertical)
+            { 
+                if(direct == left)
+                {
+                    man.Left -= var;
+                    Console.WriteLine("Lmove");
+                    pow += g;
+                    man.Top -= pow;
+                    if (man.Top >= this.Height - 100)
+                    {
+                        pow = 0;
+                        state = movent;
+                        man.Top = this.Height - man.Height - 30;
+                        direct = vertical;
+                    }
+                }
+                else 
+                {
+                    man.Left += var;
+                    Console.WriteLine("Rmove");
+                    pow += g;
+                    man.Top -= pow;
+                    if (man.Top >= this.Height - 100)
+                    {
+                        pow = 0;
+                        state = movent;
+                        man.Top = this.Height - man.Height - 40;
+                        direct = vertical;
+                    }
+                }
+            }
+           
+            foreach (PictureBox x in Controls)
+            {
+                int many = man.Top-man.Height/2;
+                if (man.Bounds.IntersectsWith(x.Bounds) && x.Tag == "floor")
+                {
+                    int py = x.Top - man.Height / 2; 
+                    if(many<py&&pow<0)
+                    {
+                        direct = vertical;
+                        state = movent;
+                        Console.WriteLine("S");
+                        man.Top = x.Top-man.Height-4;
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up&&state == storepow)
+            {
+                state = move;
+            }
+            if (e.KeyCode == Keys.Up && direct !=vertical)
+            {
+                state = move;
+            }
+        }
+    }
+}
